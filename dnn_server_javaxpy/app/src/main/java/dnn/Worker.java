009133346dff24ac2d6b14899018ad82e0e225a0
Kaiji Fu <kaiji@Kaijis-MacBook-Pro.local>
2023-01-16 17:24:26 -0500

    oh my god the server works now

--- dnn_server_javaxpy/app/src/main/java/dnn/Worker.java
+++ dnn_server_javaxpy/app/src/main/java/dnn/Worker.java
@@ -11,41 +11,56 @@ public class Worker extends Thread {
 
     public Worker() {
     }
+    public static volatile InputStream input;
+
+    public static void setInput(InputStream input) {
+        Worker.input = input;
+    }
+
+    public static InputStream getInput() {
+        return Worker.input;
+    }
+
+    public static volatile ByteArrayOutputStream buffer;
+
+    public static void setBuffer(ByteArrayOutputStream buffer) {
+        Worker.buffer = buffer;
+    }
+
+    public static ByteArrayOutputStream getBuffer() {
+        return Worker.buffer;
+    }
 
     @Override
     public void run() {
-        while (true) {    
-            try 
-            {
-                  
-                // create a new process
-                  
-                ProcessBuilder builder = new ProcessBuilder("./run");
-                Process pro = builder.start();
+        try {
+        ProcessBuilder builder = new ProcessBuilder("./run");
+        Process pro = builder.start();
 
 
-                InputStream in = pro.getInputStream();
+        InputStream in = pro.getInputStream();
 
-                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        Worker.setInput(in);
+        ProcessOutStreamReader reader = new ProcessOutStreamReader();
+        reader.start();
+        while (true) {                          
+                    // create a new process
 
-                int nRead;
-                byte[] data = new byte[16384];
+                    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                    Worker.setBuffer(buffer);
+                    int nRead;
+                    byte[] data = new byte[16777216];
 
-                while ((nRead = in.read(data, 0, data.length)) != -1) {
-                  buffer.write(data, 0, nRead);
-                }
+                    while ((nRead = in.read(data, 0, data.length)) != -1) {
+                      Worker.buffer.write(data, 0, nRead);
+                    }
 
-                String data_string = new String(buffer.toByteArray());
-
-                Server.writeData(data_string);          
-                // kill the process
-                pro.destroy();
-          
-            } 
-                catch (Exception ex) 
-            {
-                ex.printStackTrace();
             }
-        }
+    }
+    catch (Exception ex) 
+                {
+                    ex.printStackTrace();
+                }
     }
 }
+
